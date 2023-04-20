af664c01340b10cd42f2ab457aedca8a84d94098
leunami420 <marconich311@gmail.com>
2023-04-20 18:46:13 +0200

    naechster nachbar fertig, bilinear fehlt noch

--- BV2_Vorgabe/BV2_Vorgabe/src/bv_ss23/GeometricTransform.java
+++ BV2_Vorgabe/BV2_Vorgabe/src/bv_ss23/GeometricTransform.java
@@ -45,6 +45,9 @@ public class GeometricTransform {
 		for (int y = 0; y < dst.height; y++) {
 			for (int x = 0; x < dst.width; x++) {
 
+				int srcWidthMinusOne = src.width - 1;
+				int srcHeightMinusOne = src.height - 1;
+
 				int x_d= x - dst.width/2;
 				int y_d= y - dst.height/2;
 
@@ -55,17 +58,15 @@ public class GeometricTransform {
 				double y_s = y_s_s + src.height/2;
 
 				//naechster Nachbar
+				int x_s_n = (int)Math.round(x_s);
+				int y_s_n = (int)Math.round(y_s);
 
-				int OrgPos = (int)(y_s * src.width + x_s);
+				int OrgPos = (y_s_n * src.width + x_s_n);
 				int pos = y * dst.width + x;
 
-				if(x_s<0||x_s>src.width-1){
-					dst.argb[pos] = 686868;
-				}
-				else if(y_s<0||y_s>src.height-1){
-					dst.argb[pos] = 686868;
-				}
-				else{
+				if (x_s < 0 || x_s > srcWidthMinusOne || y_s < 0 || y_s > srcHeightMinusOne) {
+					dst.argb[pos] = 0x00444444;
+				} else {
 					dst.argb[pos] = src.argb[OrgPos];
 				}
 
@@ -88,6 +89,41 @@ public class GeometricTransform {
 	 * @param perspectiveDistortion amount of the perspective distortion 
 	 */
 	public void perspectiveBilinear(RasterImage src, RasterImage dst, double angle, double perspectiveDistortion) {
+
+		double angle_radiant = Math.toRadians(angle);
+		for (int y = 0; y < dst.height; y++) {
+			for (int x = 0; x < dst.width; x++) {
+
+				int srcWidthMinusOne = src.width - 1;
+				int srcHeightMinusOne = src.height - 1;
+
+				int x_d= x - dst.width/2;
+				int y_d= y - dst.height/2;
+
+				double y_s_s = y_d/(Math.cos(angle_radiant)-y_d*perspectiveDistortion*Math.sin(angle_radiant));
+				double x_s_s = x_d*(perspectiveDistortion*Math.sin(angle_radiant)*y_s_s+1);
+
+				double x_s = x_s_s + src.width/2;
+				double y_s = y_s_s + src.height/2;
+
+				//naechster Nachbar
+				int x_s_n = (int)Math.round(x_s);
+				int y_s_n = (int)Math.round(y_s);
+
+				int OrgPos = (y_s_n * src.width + x_s_n);
+				int pos = y * dst.width + x;
+
+				if (x_s < 0 || x_s > srcWidthMinusOne || y_s < 0 || y_s > srcHeightMinusOne) {
+					dst.argb[pos] = 0x00444444;
+				} else {
+					dst.argb[pos] = src.argb[OrgPos];
+				}
+
+
+
+			}
+		}
+
  		   	  	  		
 		// TODO: implement the geometric transformation using bilinear interpolation
 		
