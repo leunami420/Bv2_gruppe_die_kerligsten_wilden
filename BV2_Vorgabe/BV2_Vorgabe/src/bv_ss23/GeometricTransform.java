bc16ea6a6e495f7ad2cc4287dff9cd38b313f118
leunami420 <marconich311@gmail.com>
2023-04-20 23:41:36 +0200

    naechster nachbar fertig, bilinear habs versucht hat nicht geklappt und autoload fürs bild fehlt auch

--- BV2_Vorgabe/BV2_Vorgabe/src/bv_ss23/GeometricTransform.java
+++ BV2_Vorgabe/BV2_Vorgabe/src/bv_ss23/GeometricTransform.java
@@ -91,33 +91,76 @@ public class GeometricTransform {
 	public void perspectiveBilinear(RasterImage src, RasterImage dst, double angle, double perspectiveDistortion) {
 
 		double angle_radiant = Math.toRadians(angle);
+		int A_argb;
+		int B_argb;
+		int C_argb;
+		int D_argb;
+		int srcWidthMinusOne = src.width - 1;
+		int srcHeightMinusOne = src.height - 1;
+
 		for (int y = 0; y < dst.height; y++) {
 			for (int x = 0; x < dst.width; x++) {
 
-				int srcWidthMinusOne = src.width - 1;
-				int srcHeightMinusOne = src.height - 1;
+				int pos = y * dst.width + x;
 
 				int x_d= x - dst.width/2;
 				int y_d= y - dst.height/2;
 
-				double y_s_s = y_d/(Math.cos(angle_radiant)-y_d*perspectiveDistortion*Math.sin(angle_radiant));
-				double x_s_s = x_d*(perspectiveDistortion*Math.sin(angle_radiant)*y_s_s+1);
+				double y_s_temp = y_d/(Math.cos(angle_radiant)-y_d*perspectiveDistortion*Math.sin(angle_radiant));
+				double x_s_temp = x_d*(perspectiveDistortion*Math.sin(angle_radiant)*y_s_temp+1);
 
-				double x_s = x_s_s + src.width/2;
-				double y_s = y_s_s + src.height/2;
+				double x_s = x_s_temp + src.width/2;
+				double y_s = y_s_temp + src.height/2;
 
 				//naechster Nachbar
-				int x_s_n = (int)Math.round(x_s);
-				int y_s_n = (int)Math.round(y_s);
+				int x_s_downrounded = (int) Math.floor(x_s);
+				int x_s_uprounded = x_s_downrounded + 1;
+				int y_s_downrounded = (int) Math.floor(y_s);
+				int y_s_uprounded = y_s_downrounded + 1;
 
-				int OrgPos = (y_s_n * src.width + x_s_n);
-				int pos = y * dst.width + x;
+				double h = x_s - x_s_downrounded;
+				double v = y_s - y_s_downrounded;
 
-				if (x_s < 0 || x_s > srcWidthMinusOne || y_s < 0 || y_s > srcHeightMinusOne) {
-					dst.argb[pos] = 0x00444444;
-				} else {
-					dst.argb[pos] = src.argb[OrgPos];
+				if (x_s_downrounded >= 0 && y_s_downrounded >= 0 && x_s_downrounded < src.width && y_s_downrounded < src.height) {
+					A_argb = src.argb[y_s_downrounded * dst.width + x_s_downrounded];
+				}else {
+					A_argb = 0xFFFFFFFF;
+
 				}
+
+				if (x_s_uprounded >= 0 && y_s_downrounded >= 0 && x_s_uprounded < src.width && y_s_downrounded < src.height) {
+					B_argb = src.argb[y_s_downrounded * dst.width + x_s_uprounded];
+				}else {
+					B_argb = 0xFFFFFFFF;
+
+				}
+
+				if (x_s_downrounded >= 0 && y_s_uprounded >= 0 && x_s_downrounded < src.width && y_s_uprounded < src.height) {
+
+				C_argb = src.argb[y_s_uprounded * dst.width + x_s_downrounded];
+
+				}else {
+					C_argb = 0xFFFFFFFF;
+
+				}
+
+
+				if (x_s_uprounded >= 0 && y_s_uprounded >= 0 && x_s_uprounded < src.width && y_s_uprounded < src.height) {
+					D_argb = src.argb[y_s_uprounded * src.width + x_s_uprounded];
+				} else {
+					D_argb = 0xFFFFFFFF;
+				}
+
+				//red
+				int r = BiliniarInterpolate(h,v,(A_argb >> 16) & 0xff,(B_argb >> 16) & 0xff,(C_argb >> 16) & 0xff,(D_argb >> 16) & 0xff);
+				//green
+				int g = BiliniarInterpolate(h,v,(A_argb >> 8) & 0xff,(B_argb >> 8) & 0xff,(C_argb >> 8) & 0xff,(D_argb >> 8) & 0xff);
+				//blue
+				int b = BiliniarInterpolate(h,v,A_argb & 0xff,B_argb & 0xff,C_argb & 0xff,D_argb & 0xff);
+
+
+				dst.argb[pos] = 0xFF000000 + (( r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+
 
 
 
@@ -130,6 +173,11 @@ public class GeometricTransform {
 		// NOTE: angle contains the angle in degrees, whereas Math trigonometric functions need the angle in radiant
 		
  	}
+
+	 private int BiliniarInterpolate(double h,double v,int A,int B,int C,int D){
+		double result = A*(1-h)*(1-v)+B*h*(1-v)+C*(1-h)*v+D*h*v;
+		return (int) result;
+	 }
 
  		   	  	  		
 }
